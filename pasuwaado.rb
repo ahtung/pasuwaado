bb0be67b7c9a3e241a33895af3db6119037cb7ec
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-05 10:43:25 +0100

    V1.0

--- pasuwaado.rb
+++ pasuwaado.rb
@@ -3,7 +3,7 @@ require "thor"
 class PasuwaadoCLI < Thor
   desc 'Get password for site', ''
   def pass(url)
-    puts "#{url}"
+    puts "dunya386925#{url.match(/\/\/w{3}?\.?(.+)\./).captures.first.upcase}"
   end
 end
 
