57881fbd803ceb45f9469987b2b632790bcff5be
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-07 00:39:13 +0100

    improve cli usability

--- lib/pasuwaado/cli.rb
+++ lib/pasuwaado/cli.rb
@@ -3,7 +3,7 @@ require 'thor'
 
 module Pasuwaado
   class CLI < Thor
-    desc 'Get password for site', ''
+    desc 'pass <url>', 'Get password for site'
     def pass(url)
       say "looking up pass for #{url}", :blue
       result = "dunya386925#{url.match(/\/\/w{3}?\.?(.+)\./).captures.first.capitalize}"
